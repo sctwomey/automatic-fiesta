a0537715fde0f4808cd0e399497afc834d2312c6
sctwomey <sctwomey@gmail.com>
2023-11-05 17:09:50 -0800

    Modify the connection js file

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 const { connect, connection } = require("mongoose");
 
+// This is for connecting the server to the database, and for future deployment, if needed.
 connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialDB');
 
 module.exports = connection;
