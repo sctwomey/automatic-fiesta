2ffe20388880c6f1e82dce790474003f3102bf90
sctwomey <sctwomey@gmail.com>
2023-11-05 17:07:45 -0800

    Modify the models index js file

--- models/index.js
+++ models/index.js
@@ -1,6 +1,4 @@
-// These requires are for all of the models.
 const User = require('./User');
 const Thought = require('./Thought');
 
-// This export's an object containing all of the models.
 module.exports = { User, Thought };
